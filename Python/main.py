61d682d7018fc06fd3012b175dca913085e5b586
Felipe Castellanos <felipe.programmer1@gmail.com>
2022-08-17 15:30:57 -0500

    Agregando las clases

---
+++ Python/main.py
@@ -0,0 +1,12 @@
+from car import Car
+
+print("Hola Mundo")
+car = Car()
+car.license = "AMS234"
+car.driver = "Andres Herrera"
+print(vars(car))
+
+car2 = Car()
+car2.license = "QWE567"
+car2.driver = "Matha"
+print(vars(car2))
